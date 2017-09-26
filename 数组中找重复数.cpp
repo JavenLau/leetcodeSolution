269e5cfc1ca24a807bf69dfeaf6184895c38e7a5
JavenLau <javenlau@hotmail.com>
2017-09-26 21:48:35 +0800

    add

--- 数组中找重复数.cpp
+++ 数组中找重复数.cpp
@@ -2,17 +2,20 @@
 //http://blog.csdn.net/u010579068/article/details/49514745
 
 #include <stdio.h>
-#include<algorithm> 
+#include<algorithm>
 
 using namespace std;
 
+//solution 1
 bool dulplicate(int num[], int len, int *dul)
 {
 	if(len <= 1)	return false;
-	
+
 	for(int i=0; i<len; i++)
 	{
 		if(num[i]<0 || num[i]>len)	return false;
+
+
 		while(num[i] != i)
 		{
 			if(num[i] == num[num[i]] )
@@ -22,8 +25,48 @@ bool dulplicate(int num[], int len, int *dul)
 			}
 			swap(num[i], num[num[i]]);
 		}
+
 	}
 }
+
+//solution 2 哈希表
+bool dulplicate(int num[], int len, int *dul)
+{
+	if(len <= 1)	return false;
+
+	int hash[100] = {0};
+	for(int i = 0; i < len; i++)
+	{
+		hash[num[i]]++;
+		if(hash[num[i]] > 1)//如果出现的次数大于1则返回
+		{
+			*dul = num[i];
+			return true;
+		}
+	}
+}
+
+//solution 3 排序以后检查
+bool dulplicate(int num[], int len, int *dul)
+{
+	if(len <= 1)	return false;
+
+	for(int i=0; i<len; i++)
+	{
+		if(num[i]<0 || num[i]>len)	return false;
+
+		sort(num, num + len);
+		for(int i = 0; i < len-1; i++)
+		{
+			if(num[i] == num[i+1])
+			{
+				*dul = num[i];
+				return true;
+			}
+		}
+	}
+}
+
 
 int main()
 {
