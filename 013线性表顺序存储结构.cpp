485fdd246ba55a3628513ed15014a81c107c446d
JavenLau <javenlau@hotmail.com>
2017-10-31 21:58:08 +0800

    add

--- 013线性表顺序存储结构.cpp
+++ 013线性表顺序存储结构.cpp
@@ -1,48 +1,69 @@
 
 
-/* 线性表顺序存储结构 */
+/* 线性表链式存储结构--单链表 */
 typedef struct
 {
-	int data[max];
-	int len;
-}sqlist;
+	int data;
+	struct node *next;
+}node, *linklist;
 
-/* 线性表顺序存储结构--插入 */
-Status ListInsert(SqList *L,int i,ElemType e)
+/* 线性表链式存储结构--单链表插入 */
+Status ListInsert(LinkList *L,int i,ElemType e)
 { 
-	int k;
-	if (L->length==MAXSIZE)  /* 顺序线性表已经满 */
-		return ERROR;
-	if (i<1 || i>L->length+1)/* 当i比第一位置小或者比最后一位置后一位置还要大时 */
-		return ERROR;
-
-	if (i<=L->length)        /* 若插入数据位置不在表尾 */
+	int j;
+	LinkList p,s;
+	p = *L;   
+	j = 1;
+	while (p && j < i)     /* 寻找第i个结点 */
 	{
-		for(k=L->length-1;k>=i-1;k--)  /* 将要插入位置之后的数据元素向后移动一位 */
-			L->data[k+1]=L->data[k];
-	}
-	L->data[i-1]=e;          /* 将新元素插入 */
-	L->length++;
-
+		p = p->next;
+		++j;
+	} 
+	if (!p || j > i) 
+		return ERROR;   /* 第i个元素不存在 */
+	s = (LinkList)malloc(sizeof(Node));  /*  生成新结点(C语言标准函数) */
+	s->data = e;  
+	s->next = p->next;      /* 将p的后继结点赋值给s的后继  */
+	p->next = s;          /* 将s赋值给p的后继 */
 	return OK;
 }
 
-/* 线性表顺序存储结构--删除 */
-Status ListDelete(SqList *L,int i,ElemType *e) 
+/* 线性表链式存储结构--单链表删除 */
+Status ListDelete(LinkList *L,int i,ElemType *e) 
 { 
-    int k;
-    if (L->length==0)               /* 线性表为空 */
-		return ERROR;
-    if (i<1 || i>L->length)         /* 删除位置不正确 */
-        return ERROR;
-    *e=L->data[i-1];
-    if (i<L->length)                /* 如果删除不是最后位置 */
-    {
-        for(k=i;k<L->length;k++)/* 将删除位置后继元素前移 */
-			L->data[k-1]=L->data[k];
-    }
-    L->length--;
-    return OK;
+	int j;
+	LinkList p,q;
+	p = *L;
+	j = 1;
+	while (p->next && j < i)	/* 遍历寻找第i个元素 */
+	{
+        p = p->next;
+        ++j;
+	}
+	if (!(p->next) || j > i) 
+	    return ERROR;           /* 第i个元素不存在 */
+	q = p->next;
+	p->next = q->next;			/* 将q的后继赋值给p的后继 */
+	*e = q->data;               /* 将q结点中的数据给e */
+	free(q);                    /* 让系统回收此结点，释放内存 */
+	return OK;
 }
 
 
+/* 线性表链式存储结构--双向链表 */
+typedef struct dulnode
+{
+	int data;
+	struct dulnode *prior;
+	struct dulnode *next;
+}dulnode, *dullinklist;
+
+/* 线性表链式存储结构--双向链表插入 */
+s->prior = p;
+s->next = p->next;
+p->next->prior = s;
+p->next =  s;
+
+/* 线性表链式存储结构--双向链表删除 */
+p->next->prior = p->prior;
+p->prior->next = p->next;
