cbfaff95c4fdcaacd2396d4b0c28387ece2c4447
JavenLau <javenlau@hotmail.com>
2017-10-31 21:15:08 +0800

    add

---
+++ 013线性表顺序存储结构.cpp
@@ -0,0 +1,48 @@
+
+
+/* 线性表顺序存储结构 */
+typedef struct
+{
+	int data[max];
+	int len;
+}sqlist;
+
+/* 线性表顺序存储结构--插入 */
+Status ListInsert(SqList *L,int i,ElemType e)
+{ 
+	int k;
+	if (L->length==MAXSIZE)  /* 顺序线性表已经满 */
+		return ERROR;
+	if (i<1 || i>L->length+1)/* 当i比第一位置小或者比最后一位置后一位置还要大时 */
+		return ERROR;
+
+	if (i<=L->length)        /* 若插入数据位置不在表尾 */
+	{
+		for(k=L->length-1;k>=i-1;k--)  /* 将要插入位置之后的数据元素向后移动一位 */
+			L->data[k+1]=L->data[k];
+	}
+	L->data[i-1]=e;          /* 将新元素插入 */
+	L->length++;
+
+	return OK;
+}
+
+/* 线性表顺序存储结构--删除 */
+Status ListDelete(SqList *L,int i,ElemType *e) 
+{ 
+    int k;
+    if (L->length==0)               /* 线性表为空 */
+		return ERROR;
+    if (i<1 || i>L->length)         /* 删除位置不正确 */
+        return ERROR;
+    *e=L->data[i-1];
+    if (i<L->length)                /* 如果删除不是最后位置 */
+    {
+        for(k=i;k<L->length;k++)/* 将删除位置后继元素前移 */
+			L->data[k-1]=L->data[k];
+    }
+    L->length--;
+    return OK;
+}
+
+
