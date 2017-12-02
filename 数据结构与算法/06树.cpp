f55396abb1d24fbb00c4f04c9adaef6406e93fa9
JavenLau <javenlau@hotmail.com>
2017-12-03 01:19:37 +0800

    modify

--- 数据结构与算法/06树.cpp
+++ 数据结构与算法/06树.cpp
@@ -1,5 +1,12 @@
 
 
+typedef struct biNode
+{
+	int data;
+	struct biTree *lchild, *rchild;
+}biNode, *biTree;
+
+//初始化二叉树
 void createBiTree(biTree *T)
 {	
 	int index;
@@ -17,4 +24,21 @@ void createBiTree(biTree *T)
 		createBiTree(&(T->lchild));
 		createBiTree(&(T->rchild));
 	}
+}
+
+//返回二叉树深度
+int biTreeDepth(biTree *T)
+{
+	int i,j;
+	if(!T)
+		return 0;
+	if(T->lchild)
+		i = biTreeDepth(T->lchild);
+	else 
+		i = 0;
+	if(T->rchild)
+		j = biTreeDepth(T->rchild);
+	else 
+		j = 0;
+	return i>j ? i+1:j+1;
 }
